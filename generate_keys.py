66a43e9807a6efa39bba75b75ae86e9e5d05d4f4
PedroKiche <pedrohenriquekiche@gmail.com>
2024-06-04 20:22:21 -0300

    generated keys

---
+++ generate_keys.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+public_key = private_key.public_key()
+
+# Serialize the private key to PEM format
+private_key_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+# Serialize the public key to PEM format
+public_key = private_key.public_key()
+pem = public_key.public_bytes(
+   encoding=serialization.Encoding.PEM,
+   format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Write the serialized private key to a file
+with open("private_key.pem", "wb") as file:
+    file.write(private_key_pem)
+
+# Write the serialized public key to a file
+with open("public_key.pem", "wb") as file:
+    file.write(pem)
