fd847b556ac1f5af806152f4b484c721c5a44b7e
PedroKiche <pedrohenriquekiche@gmail.com>
2024-06-04 01:39:16 -0300

    teste não funcional

--- backend.py
+++ backend.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
+from flask_sse import sse
+import time
 import json
 import os
 
 app = Flask(__name__)
 CORS(app)
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
 
 DATA_FILE = 'produtos.json'
 
@@ -17,6 +21,16 @@ def read_produtos():
 def write_produtos(produtos):
     with open(DATA_FILE, 'w') as file:
         json.dump(produtos, file)
+
+@app.route('/events')
+def stream():
+    def event_stream():
+        while True:
+        # Simulando um evento a cada segundo
+            time.sleep(1)
+            yield 'data: {}\n\n'.format('Evento do servidor')
+    
+    return Response(event_stream(), mimetype='text/event-stream')
 
 # Rota para obter todos os produtos
 @app.route('/produtos', methods=['GET'])
@@ -49,6 +63,8 @@ def update_produto(produto_id):
     if produto:
         produto.update(update_data)
         write_produtos(produtos)
+        if produto['estoque'] == 0:
+            sse.publish({"message": f'O estoque do produto {produto["nome"]} chegou a 0.'}, type='publish')
         return jsonify(produto)
     return ('', 404)
 
